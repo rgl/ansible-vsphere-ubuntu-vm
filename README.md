efc1e1cdddf3c17d0ad6f67d95eeecb9a949d40f
Rui Lopes <rgl@ruilopes.com>
2024-03-20 22:24:47 +0000

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+# About
+
+This is an example Ansible project that creates a Ubuntu VMware vSphere Virtual Machine.
+
+For a more complete Ansible Playbook see the [rgl/my-ubuntu-ansible-playbooks repository](https://github.com/rgl/my-ubuntu-ansible-playbooks).
+
+# Usage
+
+Execute the following procedure in a Ubuntu machine.
+
+Install the [test/templates/ubuntu-22.04-amd64-vsphere virtual machine template](https://github.com/rgl/ubuntu-vagrant) in your VMware vSphere environment.
+
+Install Docker.
+
+Open the [inventory file](inventory.yml) and modify the virtual machines details to fit your environment.
+
+Set your VMware vSphere details:
+
+```bash
+cat >secrets.sh <<'EOF'
+export VM_VSPHERE_HOSTNAME='vsphere.local'
+export VM_VSPHERE_USERNAME='administrator@vsphere.local'
+export VM_VSPHERE_PASSWORD='password'
+export VM_VSPHERE_DATACENTER='Datacenter'
+export VM_VSPHERE_CLUSTER='Cluster'
+export VM_VSPHERE_DATASTORE='Datastore'
+export VM_VSPHERE_TEMPLATE='test/templates/ubuntu-22.04-amd64-vsphere'
+export VM_VSPHERE_FOLDER='test'
+export VM_ADMIN_PASSWORD='admin'
+export VM_GATEWAY='10.0.0.1'
+export VM_FIRST_IP='10.0.0.11'
+EOF
+source secrets.sh
+```
+
+Lint the `example.yml` playbook:
+
+```bash
+./ansible-lint.sh --offline --parseable example.yml
+```
+
+Run the `example.yml` playbook against the `example1` machine:
+
+```bash
+./ansible-playbook.sh --limit=example1 example.yml | tee ansible.log
+```
---
+++ ansible.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -euo pipefail
+
+command="$(basename "$0" .sh)"
+
+# build the ansible image.
+install -d tmp
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.ansible --iidfile tmp/ansible-iid.txt .
+ansible_iid="$(cat tmp/ansible-iid.txt)"
+
+# show information about the execution environment.
+docker run --rm -i "$ansible_iid" bash <<'EOF'
+exec 2>&1
+set -euxo pipefail
+cat /etc/os-release
+ansible --version
+python3 -m pip list
+ansible-galaxy collection list
+EOF
+
+# execute command (e.g. ansible-playbook).
+# NB the GITHUB_ prefixed environment variables are used to trigger ansible-lint
+#    to annotate the GitHub Actions Workflow with the linting violations.
+#    see https://github.com/ansible/ansible-lint/blob/v24.2.1/src/ansiblelint/app.py#L94-L95
+#    see https://ansible-lint.readthedocs.io/en/latest/usage/#ci-cd
+exec docker run \
+    --rm \
+    --net=host \
+    -v "$PWD:/project:ro" \
+    -e GITHUB_ACTIONS \
+    -e GITHUB_WORKFLOW \
+    -e VM_ANSIBLE_PROJECT_PATH="$PWD" \
+    -e VM_VSPHERE_HOSTNAME \
+    -e VM_VSPHERE_HOSTNAME \
+    -e VM_VSPHERE_USERNAME \
+    -e VM_VSPHERE_PASSWORD \
+    -e VM_VSPHERE_DATACENTER \
+    -e VM_VSPHERE_CLUSTER \
+    -e VM_VSPHERE_RESOURCE_POOL \
+    -e VM_VSPHERE_DATASTORE \
+    -e VM_VSPHERE_TEMPLATE \
+    -e VM_VSPHERE_FOLDER \
+    -e VM_GATEWAY \
+    -e VM_FIRST_IP \
+    -e VM_ADMIN_PASSWORD \
+    "$ansible_iid" \
+    "$command" \
+    "$@"
